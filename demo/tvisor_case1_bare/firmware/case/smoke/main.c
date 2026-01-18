ec63742044b238ba02c2dc5079694adaee054fca
tangf <1939261764@qq.com>
2026-01-18 15:25:45 +0800

    update code

--- demo/tvisor_case1_bare/firmware/case/smoke/main.c
+++ demo/tvisor_case1_bare/firmware/case/smoke/main.c
@@ -7,9 +7,11 @@
 volatile int a = 5;
 volatile int b;
 
+extern void uart_putc(uart_t *periph,char ch);
+
 int virt_puts(char *str){
     while(*str++){
-        *(volatile uint32_t *)0x10000000 = str[-1];
+        uart_putc((uart_t *)0x10000000,str[-1]);
     }
     return 0;
 }
--- demo/tvisor_case1_bare/main.c
+++ demo/tvisor_case1_bare/main.c
@@ -169,13 +169,13 @@ void task_1_main( void * arg ){
     // virt_puts("printf from virtual machine 0\r\n");
     tvisor_printf("instr = %08x\r\n",tvisor_vm_read32(0x80000000));
     vTaskExitCritical();
-    // tvisor_vm_memcpy((void *)0x80000000,__firmware_build_smoke_main_bin,__firmware_build_smoke_main_bin_len);
-    // for(int i = 0;i<__firmware_build_smoke_main_bin_len;i++){
-    //     if(__firmware_build_smoke_main_bin[i] != tvisor_vm_read8(0x80000000 + i)){
-    //         while(1);
-    //     }
-    // }
-    // tvisor_vm_run(&vm_ctx);
+    tvisor_vm_memcpy((void *)0x80000000,__firmware_build_smoke_main_bin,__firmware_build_smoke_main_bin_len);
+    for(int i = 0;i<__firmware_build_smoke_main_bin_len;i++){
+        if(__firmware_build_smoke_main_bin[i] != tvisor_vm_read8(0x80000000 + i)){
+            while(1);
+        }
+    }
+    tvisor_vm_run(&vm_ctx);
     while(1){
         tvisor_printf("task_1_main:%ld,mode = %d\n",read_csr(sscratch),uxTaskCurrentPrvModeGet());
         sbi_print("task_1_main:hello opensbi!\n");
